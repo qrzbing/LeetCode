0c3442ca7b028cdfa7f5f0738ea54704c25501e6
qrzbing <qrzbing@gmail.com>
2021-12-22 17:10:15 +0800

    [SOLVE] solve 20 with cpp

--- codes/cpp/20.cpp
+++ codes/cpp/20.cpp
@@ -1,71 +1,53 @@
+#include "global.hpp"
 
-// Definition for singly-linked list.
-#include <iostream>
-using namespace std;
-
-struct ListNode
+class Solution
 {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+public:
+    bool isValid(string s)
+    {
+        stack<char> sc;
+        for (char ch : s)
+        {
+            switch (ch)
+            {
+            case '(':
+                sc.push('(');
+                break;
+            case '[':
+                sc.push('[');
+                break;
+            case '{':
+                sc.push('{');
+                break;
+            case ')':
+                if (sc.empty() || sc.top() != '(')
+                    return false;
+                sc.pop();
+                break;
+            case ']':
+                if (sc.empty() || sc.top() != '[')
+                    return false;
+                sc.pop();
+                break;
+            case '}':
+                if (sc.empty() || sc.top() != '{')
+                    return false;
+                sc.pop();
+                break;
+
+            default:
+                break;
+            }
+        }
+        if (!sc.empty())
+            return false;
+        return true;
+    }
 };
-
-ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
-{
-    ListNode *lh = nullptr, *lm = nullptr;
-    int inc = 0;
-    while (l1 || l2)
-    {
-        ListNode *lt = new ListNode;
-        if (!lh)
-        {
-            lh = lt;
-            lm = lt;
-        }
-        else
-        {
-            lm->next = lt;
-            lm = lt;
-        }
-        lm->val = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + inc;
-        inc = 0;
-        if (lm->val > 10)
-        {
-            lm->val -= 10;
-            inc = 1;
-        }
-        if (l1)
-            l1 = l1->next;
-        if (l2)
-        {
-            l2 = l2->next;
-        }
-    }
-    if (inc)
-    {
-        ListNode *lt = new ListNode;
-        lt->val = 1;
-        lm->next = lt;
-    }
-    else
-    {
-        lm->next = nullptr;
-    }
-    return lh;
-}
 
 int main()
 {
-    ListNode *t1 = new ListNode;
-    ListNode *t2 = new ListNode;
-    t1->val = 7, t1->next = nullptr;
-    t2->val = 7, t2->next = nullptr;
-    ListNode *t3 = addTwoNumbers(t1, t2);
-    while (t3 != nullptr)
-    {
-        cout << t3->val << " ";
-        t3 = t3->next;
-    }
+    Solution sol;
+    cout << (sol.isValid("([)]") ? "true\n" : "false\n");
+    cout << (sol.isValid("{[]}") ? "true\n" : "false\n");
 }
